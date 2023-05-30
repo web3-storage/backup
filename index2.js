557f3bb0cdca8b397aa39c83c5c6c056198654cf
Alan Shaw <alan.shaw@protocol.ai>
2023-05-30 16:40:16 +0100

    feat: local verify

--- index2.js
+++ index2.js
@@ -1,5 +1,5 @@
 import debug from 'debug'
-import { S3Client, HeadObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client, HeadObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3'
 import retry from 'p-retry'
 import { transform } from 'streaming-iterables'
 import { pipe } from 'it-pipe'
@@ -8,18 +8,21 @@ import { CID } from 'multiformats'
 import { request } from 'undici'
 import * as raw from 'multiformats/codecs/raw'
 import * as dagPB from '@ipld/dag-pb'
+import { HashingLinkIndexer } from 'linkdex/hashing-indexer.js'
+import { CarBlockIterator } from '@ipld/car'
 import { IpfsClient } from './ipfs-client.js'
 import { createHealthCheckServer } from './health.js'
 import { fetchCID, exportCar, s3Upload, bucketKey } from './index.js'
 
 const fmt = formatNumber()
 
-const CONCURRENCY = 5
+const CONCURRENCY = 1
 const BLOCK_TIMEOUT = 1000 * 30 // timeout if we don't receive a block after 30s
 const REPORT_INTERVAL = 1000 * 10 // log download progress every 10 seconds
-const VERIFIER_CONCURRENCY = 10
-/** Max DAG size to verify (we can only do around 20GiB in 15 minutes) */
-const VERIFIER_MAX_DAG_SIZE = 1024 * 1024 * 1024 * 20
+const VERIFIER_CONCURRENCY = 1
+/** Max DAG size to verify (we can only do around 20GiB via the API in 15 minutes) */
+// const VERIFIER_MAX_DAG_SIZE = 1024 * 1024 * 1024 * 20
+const VERIFIER_MAX_DAG_SIZE = Infinity
 /** Max time to verify a DAG (in ms) */
 const VERIFIER_TIMEOUT = 1000 * 60 * 15
 
@@ -78,10 +81,37 @@ export async function startBackup ({ dataURL, s3Region, s3BucketName, s3AccessKe
     return isAlreadyStored(s3, s3BucketName, cid)
   }
 
+  /** @param {string} cid */
+  const verifyDAG = async cid => {
+    const index = new HashingLinkIndexer()
+    const res = await retry(() => s3.send(new GetObjectCommand({
+      Key: bucketKey(cid),
+      Bucket: s3BucketName,
+    })), { retries: 5 })
+    let bytes = 0
+    let count = 0
+    const intervalID = setInterval(() => log(`decoded ${fmt(count)} blocks (${fmt(bytes)} bytes) of ${cid}`), 10_000)
+    try {
+      // @ts-expect-error
+      const carBlocks = await CarBlockIterator.fromIterable(res.Body)
+      for await (const block of carBlocks) {
+        const result = index.decodeAndIndex(block)
+        if (result instanceof Promise) {
+          await result
+        }
+        count++
+        bytes += block.bytes.length
+      }
+      return index.report()
+    } finally {
+      clearInterval(intervalID)
+    }
+  }
+
   await pipe(
     fetchCID(dataURL, log),
     checkSize(ipfs, concurrency ?? CONCURRENCY, log),
-    filterVerifiedComplete(verifierURL, VERIFIER_CONCURRENCY, isExists, log),
+    filterVerifiedComplete(VERIFIER_CONCURRENCY, verifyDAG, isExists, log),
     transform(concurrency ?? CONCURRENCY, async (item) => {
       log(`processing ${item.cid}`)
       try {
@@ -127,12 +157,12 @@ export async function startBackup ({ dataURL, s3Region, s3BucketName, s3AccessKe
 }
 
 /**
- * @param {string|URL} url Verifier API URL
  * @param {number} concurrency
+ * @param {(cid: string) => Promise<import('linkdex').Report & import('linkdex/hashing-indexer').HashReport>} verifyDAG
  * @param {(cid: string) => Promise<boolean>} isExists
  * @param {(...args: any[]) => void} log
  */
-function filterVerifiedComplete (url, concurrency, isExists, log) {
+function filterVerifiedComplete (concurrency, verifyDAG, isExists, log) {
   /** @param {import('it-pipe').Source<InputData & { size?: number | undefined }>} source */
   return async function * (source) {
     yield * pipe(
@@ -153,7 +183,7 @@ function filterVerifiedComplete (url, concurrency, isExists, log) {
         const start = Date.now()
         while (true) {
           try {
-            const res = await verifyCID(url, CID.parse(item.cid), log)
+            const res = await verifyDAG(item.cid)
             log(`${item.cid} is verified ${res.structure} in S3`)
             if (!res.hashPassed) {
               log(`${item.cid} no hashes verified`)
@@ -243,10 +273,10 @@ async function getSize (ipfs, cid) {
 
 /**
  * @param {URL|string} url
- * @param {import('multiformats').Link} cid
+ * @param {string} cid
  * @param {(...args: any[]) => void} [log]
  */
-export async function verifyCID (url, cid, log = () => {}) {
+export async function verifyDAG (url, cid, log = () => {}) {
   let i = 0
   const verifyInterval = setInterval(() => log(`still verifying ${cid} after ${++i} minute(s)`), 60_000)
   try {
